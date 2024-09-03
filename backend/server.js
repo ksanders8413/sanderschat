282a5ed614cd257b611e0ff2fd69961a81b66c56
Kevin sanders <ksanders8413@yahoo.com>
2024-09-03 05:36:42 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,8 @@ import { app,server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()) //to parce incoming requests withJSON payloads from req.body
@@ -21,8 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("hello broooo");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
